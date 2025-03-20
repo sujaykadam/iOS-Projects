7cdbac912045eaae473a2600d4babe0b41442827
Sujay Kadam <skadam14@asu.edu>
2025-03-20 14:31:17 -0700

    First Project

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Sujay Kadam on 3/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
